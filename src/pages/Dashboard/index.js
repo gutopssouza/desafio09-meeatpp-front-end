e721a2be5a93cae58d46e1dd7b15d665c23ee176
Augusto <gutopssouza@gmail.com>
2019-09-12 19:16:21 -0300

    :rocket: Implement load user organized meetups on dashboard page using the api from back-end :art: Create style for dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Meetup } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [meetups, setMeetups] = useState([]);
+  const date = new Date();
+
+  useEffect(() => {
+    async function loadMeetups() {
+      const response = await api.get('organizing', {});
+
+      const data = response.data.map(meetup => {
+        const dateFormatted = format(
+          parseISO(meetup.date),
+          "d 'de' MMMM', ás' HH'h'",
+          {
+            locale: pt,
+          }
+        );
+
+        return {
+          ...meetup,
+          formattedDate: dateFormatted,
+        };
+      });
+
+      setMeetups(data);
+    }
+
+    loadMeetups();
+  }, [date]);
+
+  return (
+    <Container>
+      <header>
+        <strong>Meus meetups</strong>
+        <button type="button">
+          <div>
+            <MdAddCircleOutline size={24} color="#FFF" />
+          </div>
+          Novo meetup
+        </button>
+      </header>
+
+      <ul>
+        {meetups.map(meetup => (
+          <Meetup key={meetup.id}>
+            <strong>{meetup.title}</strong>
+            <aside>
+              <span>{meetup.formattedDate}</span>
+              <button type="button">
+                <MdChevronRight size={36} color="#FFF" />
+              </button>
+            </aside>
+          </Meetup>
+        ))}
+      </ul>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      border: 0;
+      background: #d44059;
+      border-radius: 4px;
+      height: 42px;
+      width: 172px;
+      display: flex;
+      align-items: center;
+      color: #fff;
+      justify-content: center;
+      font-weight: bold;
+
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.08, '#d44059')};
+      }
+
+      div {
+        margin-top: 5px;
+        margin-right: 10px;
+      }
+    }
+
+    strong {
+      color: #fff;
+      font-size: 32px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-gap: 10px;
+    margin-top: 30px;
+  }
+`;
+
+export const Meetup = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: rgba(0, 0, 0, 0.1);
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  strong {
+    color: #fff;
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+
+    span {
+      color: #fff;
+      font-size: 16px;
+      font-weight: normal;
+      margin: 0 15px;
+    }
+
+    button {
+      border: 0;
+      background: none;
+      margin-top: 4px;
+    }
+  }
+`;
